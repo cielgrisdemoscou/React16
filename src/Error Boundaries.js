593451d8a7e8f6f9dad023f0e74d748899eaa009
cielgrisdemoscou <hautcielbleu@gmail.com>
2019-12-24 03:42:01 +0000

    #2019-12-24 HOC

---
+++ src/Error Boundaries.js
@@ -0,0 +1,41 @@
+import React, { Component, Fragment } from "react";
+import { createPortal } from "react-dom";
+
+class ErrorMaker extends Component {
+  state = {
+    friends: ["jisu", "flynn", "daal", "kneeprayer"]
+  };
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      });
+    }, 2000);
+  };
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
+}
+
+const ErrorFallback = () => " Sorry something went wrong";
+
+class App extends Component {
+  state = {
+    hasError: false
+  };
+  componentDidCatch = (error, info) => {
+    this.setState({
+      hasError: true
+    });
+  };
+  render() {
+    const { hasError } = this.state;
+    return (
+      <Fragment>
+        <ReturnTypes />
+        <Portals />
+        {hasError ? <ErrorFallback /> : <ErrorMaker />}
+      </Fragment>
+    );
+  }
